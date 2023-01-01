537dc10777f748b31cf05093a3f7e6b6ee978f47
Jason Marshall <jasonnz@gmail.com>
2023-01-01 15:56:02 +1300

    What Happens When You Disconnect?

--- server.js
+++ server.js
@@ -14,21 +14,25 @@ let readyPlayerCount = 0;
 
 io.on('connection', (socket) => {
 	console.log('A user connected... ', socket.id);
-	
+
 	socket.on('ready', () => {
 		console.log('Player is ready... ', socket.id);
 		readyPlayerCount++;
 
-		if (readyPlayerCount === 2) {
+		if (readyPlayerCount % 2 === 0) {
 			io.emit('startGame', socket.id);
 		}
 	});
-	
+
 	socket.on('paddleMove', (paddleData) => {
 		socket.broadcast.emit('paddleMove', paddleData);
 	});
 
 	socket.on('paddleMove', (ballData) => {
 		socket.broadcast.emit('paddleMove', ballData);
+	});
+
+	socket.on('disconnect', (reason) => {
+		console.log(`Client ${socket.id} disconnected due to: ${reason}`);
 	});
 });
