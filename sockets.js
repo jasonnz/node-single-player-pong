b41e14da65918e5154705e7287b73e772d85e575
Jason Marshall <jasonnz@gmail.com>
2023-01-09 15:21:36 +1300

    Rooms

--- sockets.js
+++ sockets.js
@@ -4,26 +4,29 @@ function listen(io) {
 	const pongNameSpace = io.of('/pong');
 	pongNameSpace.on('connection', (socket) => {
 		console.log('A user connected... ', socket.id);
-
+		let room;
 		socket.on('ready', () => {
+			room = 'room' + Math.floor(readyPlayerCount / 2);
+			socket.join(room);
 			console.log('Player is ready... ', socket.id);
 			readyPlayerCount++;
 
 			if (readyPlayerCount % 2 === 0) {
-				pongNameSpace.emit('startGame', socket.id);
+				pongNameSpace.in(room).emit('startGame', socket.id);
 			}
 		});
 
 		socket.on('paddleMove', (paddleData) => {
-			socket.broadcast.emit('paddleMove', paddleData);
+			socket.to(room).emit('paddleMove', paddleData);
 		});
 
 		socket.on('paddleMove', (ballData) => {
-			socket.broadcast.emit('paddleMove', ballData);
+			socket.to(room).emit('paddleMove', ballData);
 		});
 
 		socket.on('disconnect', (reason) => {
 			console.log(`Client ${socket.id} disconnected due to: ${reason}`);
+			socket.leave(room);
 		});
 	});
 }
